dd2caecddbd738b8e6c3d02747d5a0eee8602a5d
Corey <Corey255A1@hotmail.com>
2018-01-20 21:18:51 -0500

    Changes to make it more robust
    The image folder I was testing it on had some weird nonimages that were getting through and causing the code to crash. Added the Try block. Made the directory format without the \ at the end.

--- ImageFinder/ImageFinder/ImageFinder.py
+++ ImageFinder/ImageFinder/ImageFinder.py
@@ -12,10 +12,10 @@ AcceptableImages = ['jpeg','png','gif','bmp']
 #to see that one
 
 ImageToFind = "I:\\Pictures\\S5\\20140614_161829.jpg"
-LocationToLook = "I:\\Pictures\\S5\\cache\\"
+LocationToLook = "I:\\Pictures\\S5\\cache"
 Recurse = True
 
-DifferenceValue = 0.05
+DifferenceValue = 0.02
 NormalizedScale = 1080
 
 if len(sys.argv)==3:
@@ -25,7 +25,10 @@ elif len(sys.argv)==4:
     if sys.argv[1] == '-r':
         Recurse = True
     ImageToFind = sys.argv[2]
+    ImageToFind = ImageToFind.replace("\"","")
     LocationToLook = sys.argv[3]
+    LocationToLook = LocationToLook.replace("\"","")
+
 
 def GetScale(img, size):
     (h,w) = img.shape[:2]
@@ -35,6 +38,10 @@ def GetScale(img, size):
 
 def FindImages(dir, recurse):
     topDirs = os.listdir(dir)
+    print("Looking in: " + dir)
+    total = str(len(topDirs));
+    print("--" + str(len(topDirs))+ "-- files")
+    c = 0;
     for fd in topDirs:
         if os.path.isdir(dir+fd):
             if recurse:
@@ -42,22 +49,37 @@ def FindImages(dir, recurse):
                 continue
             else:
                 continue
-        imgPath = dir+fd;
-        type = imghdr.what(imgPath)
-        if type in AcceptableImages:
-            compare = cv2.imread(imgPath,0)
-            (compare,cW,cH) = GetScale(compare,NormalizedScale)
-            if cW==oW and cH==oH:
-                normVal = cv2.norm(OrigImage,compare)/(cW*cH)
-                if normVal < DifferenceValue:
-                    print("FOUND: " + imgPath)
-                    print("\tDiff:" + str(normVal))
+        imgPath = dir+"\\"+fd;
+        try:
+            type = imghdr.what(imgPath)
+            if type in AcceptableImages:
+                compare = cv2.imread(imgPath,0)
+                if compare is None:
+                    print("Couldn't read: " + imgPath)
+                    continue
+                (compare,cW,cH) = GetScale(compare,NormalizedScale)
+                if cW==oW and cH==oH:
+                    normVal = cv2.norm(OrigImage,compare)/(cW*cH)
+                    if normVal < DifferenceValue:
+                        print("FOUND: " + imgPath)
+                        TotalImageList.append((imgPath,str(normVal)))
+                        print("\tDiff:" + str(normVal))
+        except:
+            print("Couldn't read a file")
+        c = c+1
+        if c%100==0:
+            print(str(c) + " out of " + total)
 
+TotalImageList = []
 OrigImage = cv2.imread(ImageToFind,0) #GreyScale
 (OrigImage,oW,oH) = GetScale(OrigImage,NormalizedScale)
 cv2.imshow("test",OrigImage)
 cv2.waitKey()
 topDirs = os.listdir(LocationToLook)
 FindImages(LocationToLook, Recurse)
+
+print("Image Locations:")
+for foundimg in TotalImageList:
+    print(foundimg[0] + " " + foundimg[1])
 
 
