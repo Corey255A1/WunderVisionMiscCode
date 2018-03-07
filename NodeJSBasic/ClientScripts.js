99f4096696a932f3520b9bbeb79b3bace80c5c3f
Corey <Corey255A1@hotmail.com>
2018-03-07 07:18:06 -0500

    Real time paint

---
+++ NodeJSBasic/ClientScripts.js
@@ -0,0 +1,150 @@
+﻿var socket = io(); //load socket.io-client and connect to the host that serves the page
+var theField = document.getElementById("playingfield");
+var ctx = theField.getContext("2d");
+theField.addEventListener('touchstart', onTouchStart, false);
+theField.addEventListener('touchmove', onTouchMove, false);
+var bMouseDown = false;
+function Coords() {
+    var x = 0;
+    var y = 0;
+}
+
+function ChangeChecks(black, blue, red, green)
+{
+    document.getElementById("blackCheck").checked = black;
+    document.getElementById("blueCheck").checked = blue;
+    document.getElementById("redCheck").checked = red;
+    document.getElementById("greenCheck").checked = green;
+}
+
+window.addEventListener("load", function () { //when page loads
+    socket.emit('refresh', 'rectlist');
+    document.getElementById("blackCheck").addEventListener("change", function () { //add event listener for when checkbox changes
+        socket.emit("blackChk", Number(this.checked)); //send button status to server (as 1 or 0)
+    });
+    document.getElementById("blueCheck").addEventListener("change", function () { //add event listener for when checkbox changes
+        socket.emit("blueChk", Number(this.checked)); //send button status to server (as 1 or 0)
+    });
+    document.getElementById("redCheck").addEventListener("change", function () { //add event listener for when checkbox changes
+        socket.emit("redChk", Number(this.checked)); //send button status to server (as 1 or 0)
+    });
+    document.getElementById("greenCheck").addEventListener("change", function () { //add event listener for when checkbox changes
+        socket.emit("greenChk", Number(this.checked)); //send button status to server (as 1 or 0)
+    });
+});
+
+socket.on('color', function (data) { //get button status from client
+    switch (data) {
+        case 1: ChangeChecks(1, 0, 0, 0); break;
+        case 2: ChangeChecks(0, 1, 0, 0); break;
+        case 3: ChangeChecks(0, 0, 1, 0); break;
+        case 4: ChangeChecks(0, 0, 0, 1); break;
+    }
+    
+});
+socket.on('info', function (data) {
+    document.getElementById("information").innerHTML = data;
+})
+socket.on('rect', function (data) {
+    document.getElementById("information").innerHTML = data.x + " " + data.y;
+    ctx.fillStyle = "rgb(" + data.r + ", " + data.g + ", " + data.b + ")";
+    ctx.fillRect(data.x - data.w / 2, data.y - data.h / 2, data.w, data.h);
+})
+socket.on('rectlistdata', function (data) {
+    document.getElementById("information").innerHTML = "Received UpTo Date Data from Server";
+    ctx.clearRect(0, 0, 1080, 1080);
+    for (var k in data)
+    {
+        var r = data[k];
+        if (r != null) {
+            //console.log(r.x + " " + r.y);
+            ctx.fillStyle = "rgb("+r.r+", "+r.g+", "+r.b+")";
+            ctx.fillRect(r.x - r.w / 2, r.y - r.h / 2, r.w, r.h);
+        }
+    }    
+})
+socket.on('address', function (data) {
+    document.getElementById("address").innerHTML = data;
+})
+
+function onMouseMove(e)
+{
+    if (bMouseDown)
+    {
+        var box = theField.getBoundingClientRect();
+        var r = new Coords();
+        var X = e.clientX - box.left;
+        var Y = e.clientY - box.top;
+        r.x = X;
+        r.y = Y;
+
+        socket.emit("rect", r);
+        document.getElementById("information").innerHTML = X + " " + Y;
+    }
+}
+function onMouseUp(e)
+{
+    bMouseDown = false;
+}
+function onMouseLeave(e)
+{
+    bMouseDown = false;
+}
+function onMouseDown(e)
+{
+    bMouseDown = true;
+    var box = theField.getBoundingClientRect();
+    var r = new Coords();
+    var X = e.clientX - box.left;
+    var Y = e.clientY - box.top;
+    r.x = X;
+    r.y = Y;
+
+    socket.emit("rect", r);
+    document.getElementById("information").innerHTML = X + " " + Y;
+}
+
+
+function onTouchStart(e) {
+    
+    if (e.touches)
+    {
+        //One finger
+        if (e.touches.length >= 1)
+        {
+            var t = e.touches[0];
+            //var X = t.pageX - t.target.offsetLeft;
+            //var Y = t.pageY - t.target.offsetTop;
+            var box = theField.getBoundingClientRect();
+            var X = t.pageX - box.left;
+            var Y = t.pageY - box.top;
+            var r = new Coords();
+            r.x = X;
+            r.y = Y;
+
+            socket.emit("rect", r);
+        }
+    }
+    event.preventDefault();
+    
+
+}
+function onTouchMove(e){
+    if (e.touches) {
+        //One finger
+        if (e.touches.length >= 1) {
+            var t = e.touches[0];
+            //var X = t.pageX - t.target.offsetLeft;
+            //var Y = t.pageY - t.target.offsetTop;
+            var box = theField.getBoundingClientRect();
+            var X = t.pageX- box.left;
+            var Y = t.pageY - box.top;
+            var r = new Coords();
+            r.x = X;
+            r.y = Y;
+            socket.emit("rect", r);
+        }
+    }
+    e.preventDefault();
+
+}
--- NodeJSBasic/server.js
+++ NodeJSBasic/server.js
@@ -8,11 +8,22 @@ var port = process.env.PORT || 1337;
 
 http.listen(port); //start listening on the port
 
+var validFiles = [
+    "/clientscripts.js",
+    "/index.html",
+    "/node_modules/socket.io-client/dist/socket.io.js",
+    "/css/bootstrap.css",
+    "/css/bootstrap-grid.css",
+    "/css/bootstrap-reboot.css",
+    "/css/mystyles.css"
+]
+
+
 //When using Node.js you have to make sure that you are serving all of the files
 //and their correct types.
 function handler(req, res) {
     var file = 'index.html';
-    if (req.url != '/')
+    if (req.url != '/' && validFiles.indexOf(req.url.toLowerCase()) > -1)
     {
         file = req.url;
     }
@@ -40,15 +51,112 @@ function handler(req, res) {
 
 }
 
-var PersistentCheckBoxState = 1;
+var MaxRects = 100;
+var PersistentRectangleMap = new Array(1080);
+var CurrentColor = 1; //black=1 blue=2 red=3 green=4
+function GetCurrentColor()
+{
+    switch(CurrentColor) {
+        case 1:
+            return {
+                r:0,
+                g:0,
+                b:0
+            };
+        case 2:
+            return {
+                r:0,
+                g:0,
+                b:255
+            };
+        case 3:
+            return {
+                r:255,
+                g:0,
+                b:0
+            };
+        case 4:
+            return {
+                r:0,
+                g:255,
+                b:0
+            };
+    }
+}
+function Shape(_x, _y, _w, _h, _r, _g, _b)
+{
+    return {
+        x: _x,
+        y: _y,
+        w: _w,
+        h: _h,
+        r: _r,
+        g: _g,
+        b: _b
+    }
+}
 
+for (var i = 0; i < 1080; i++)
+{
+    PersistentRectangleMap[i] = new Array(1080);
+}
 io.sockets.on('connection', function (socket) {
-    socket.emit('testCtrl',PersistentCheckBoxState)
-    socket.on('testCtrl', function (data) {
-        PersistentCheckBoxState = data;
-        console.log(PersistentCheckBoxState);        
-        socket.broadcast.emit('testCtrl', PersistentCheckBoxState); //Alert the other clients of the change
+    var thisClientIP = socket.handshake.address;
+    socket.emit('color', CurrentColor);
+    socket.emit('address', thisClientIP);
+
+    socket.on('blackChk', function (data) {
+        if (data == 1) CurrentColor = 1;
+        socket.emit('color', CurrentColor); //Send to Client 
+        socket.broadcast.emit('color', CurrentColor); //Alert the other clients of the change
     });
+    socket.on('blueChk', function (data) {
+        if (data == 1) CurrentColor = 2;
+        socket.emit('color', CurrentColor); //Send to Client
+        socket.broadcast.emit('color', CurrentColor); //Alert the other clients of the change
+    });
+    socket.on('redChk', function (data) {
+        if (data == 1) CurrentColor = 3;
+        socket.emit('color', CurrentColor); //Send to Client
+        socket.broadcast.emit('color', CurrentColor); //Alert the other clients of the change
+    });
+    socket.on('greenChk', function (data) {
+        if (data == 1) CurrentColor = 4;
+        socket.emit('color', CurrentColor); //Send to Client
+        socket.broadcast.emit('color', CurrentColor); //Alert the other clients of the change
+    });
+    socket.on('info', function (data) {
+        console.log(data);
+        socket.broadcast.emit('info', data);
+    })
+
+    socket.on('refresh', function (data)
+    {
+        if (data == 'rectlist') {
+            var ListToSend = []
+            for (var x = 0; x < PersistentRectangleMap.length; x++) {
+                for (var y = 0; y < PersistentRectangleMap[x].length; y++) {
+                    var r = PersistentRectangleMap[x][y];
+                    if (r != null) {
+                        ListToSend.push(r);
+                    }
+               }
+            }
+            socket.emit('rectlistdata', ListToSend);
+        }
+    })
+    var Size = 4;
+    socket.on('rect', function (data) {
+        //console.log(data);
+        var c = GetCurrentColor();       
+        data.x = (Math.trunc(data.x/Size)*Size);
+        data.y = (Math.trunc(data.y / Size) * Size);
+        var shape = new Shape(data.x, data.y, Size, Size, c.r, c.g, c.b);
+        PersistentRectangleMap[data.x][data.y] = shape;
+
+        socket.emit('rect', shape);
+        socket.broadcast.emit('rect', shape);        
+    })
 });
 
 //the original way I saw how to do this on the W3Schools page
