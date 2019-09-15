a745e1050394091b44ab770cd2b7028a3b19cf9a
Corey <Corey255A1@hotmail.com>
2019-09-15 11:07:47 -0400

    GZipping multiple files into a package

---
+++ GZipMulti/GZipMulti/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GZipMultiLib;
+using System.IO;
+using System.Collections.Generic;
+namespace GZipMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<FileInfo> files = new List<FileInfo>();
+            foreach(string file in Directory.EnumerateFiles("C:\\Temp\\InputFolder\\"))
+            {
+                files.Add(new FileInfo(file));
+            }
+
+            GZipFiles.Compress("C:\\Temp\\Output.package", files);
+            GZipFiles.Decompress("C:\\Temp\\Output.package", "C:\\Temp\\OutputFolder\\");
+
+            Console.WriteLine("DONE");
+
+
+        }
+    }
+}
